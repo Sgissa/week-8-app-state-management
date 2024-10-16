2726822970ce2c90d699d79d2d4bc3ad1fdf8824
Sgissa <salmanegarba721@gmail.com>
2024-10-16 10:43:50 -0400

    created hover for showcases

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,15 @@
+import pageStyles from './page.module.css'
+import ShowcaseImage from "@/components/ShowcaseImage"
+
 export default function Home(){
   return (
-    <>
-    
-    </>
+      <main >
+        <h1>The Squigly Zebra</h1>
+        <section className={pageStyles.zebraShowcaseImages}>
+            <ShowcaseImage title='On The Beach!' shortDesc='Summer 2023'/>
+            <ShowcaseImage title='Snowboarding' shortDesc='Spring 2023'/>
+            <ShowcaseImage title='Rockcliming!' shortDesc='Summer 2023'/>
+        </section>
+    </main>
   )
 }
